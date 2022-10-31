e2d7d2d375ae8fa3f62a01bfe70d397a693d708d
ap3334 <chwil2989@naver.com>
2022-10-31 16:51:08 +0900

    테스트 케이스

--- src/main/java/com/example/security_jwt/SecurityJwtApplication.java
+++ src/main/java/com/example/security_jwt/SecurityJwtApplication.java
@@ -2,12 +2,22 @@ package com.example.security_jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SecurityJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityJwtApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ src/main/java/com/example/security_jwt/member/entity/Member.java
@@ -0,0 +1,43 @@
+package com.example.security_jwt.member.entity;
+
+import com.example.security_jwt.base.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+
+    @Column(unique = true)
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    private String email;
+
+    public Member(long id) {
+        super(id);
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("MEMBER"));
+
+        return authorities;
+    }
+}
